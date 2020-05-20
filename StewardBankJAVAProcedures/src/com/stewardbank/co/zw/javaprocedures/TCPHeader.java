e94d92e7caeaca258662298ca0cf163d2d324b26
Administrator <Administrator@W2K12DEV01>
2020-05-21 00:05:20 +0530

    updated router,t24suflow,simulator

--- StewardBankJAVAProcedures/src/com/stewardbank/co/zw/javaprocedures/TCPHeader.java
+++ StewardBankJAVAProcedures/src/com/stewardbank/co/zw/javaprocedures/TCPHeader.java
@@ -4,9 +4,9 @@ public class TCPHeader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//byte [] by={30,38,31,30,82,38,00,00,02,00,00,00,04,00,00,00,00,00,00,00,30,35,32,34,30,39,30,31,33,38,37,32,36,35,34,30,31,31,30,31,33,38,30,35,32,34,30,30,33,30,31};
-	//byte[] res = new2ByteHeaderPacket(by);
-	//System.out.println(res.toString());
+		byte [] by={30,38,30,30,82,38,00,00,00,00,00,00,04,00,00,00,00,00,00,00,30,35,31,38,32,32,30,32,32,34,31,38,31,39,35,37,30,30,30,32,32,34,30,35,31,39,33,30,31};
+	byte[] res = new2ByteHeaderPacket(by);
+	System.out.println(res.toString());
 	}
 	
 	public static byte[] new2ByteHeaderPacket(byte[] data) {
